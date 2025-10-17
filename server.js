daf0721e0d848892a9e4e00d6d2d9381007e0689
sajal singh <singhsajal920@gmail.com>
2025-10-17 22:59:17 +0530

    add multiple users

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const users = [{ id: 1, username: 'admin', password: 'admin123' }];
+const users = [{ id: 1, username: 'admin', password: 'admin123' },
+  { id: 2, username: 'admin2', password: 'admin123' },
+{ id: 3, username: 'admin3', password: 'admin123' },
+{ id: 4, username: 'admin4', password: 'admin123' },
+{ id: 4, username: 'admin5', password: 'admin123' },
+{ id: 6, username: 'admin6', password: 'admin123' }];
+
 const SECRET = 'SUPER_SECRET_KEY';
 
 app.post('/login', (req, res) => {
